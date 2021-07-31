bb0efad9b54830445a3b39af7a5901b556ad4d04
Ryan Epp <re17wj@brocku.ca>
2021-07-30 22:09:52 -0400

    Update DelGen.py

--- DelGen.py
+++ DelGen.py
@@ -7,11 +7,6 @@ filesHere.remove("DelGen.py")
 
 print("Working in " + home_dir)
 
-dependencies = {
-    "h3vr.otherloader.deli": "0.3.0",
-    "h3vr.tnhtweaker.deli": "1.6.7"
-}
-
 str = """
 {
     "guid": "",
@@ -48,7 +43,9 @@ if list_authors.count != 0:
 else:
     string_author = "unknown"
 
-json_data["guid"] = "h3vr." + string_author + "." + string_name.replace(" ", "").lower()
+json_data["name"] = string_name
+json_data["description"] = string_description
+json_data["guid"] = "h3vr." + string_author.replace(" ", "").lower() + "." + string_name.replace(" ", "").lower()
 json_data["authors"] = list_authors
 
 print("Please input your mod version (x.x.x format)")
@@ -61,82 +58,67 @@ while True:
     except:
         print("This is not a correct version string, try again.")
 
-print("\nPlease select loading perameters for your files.\nAsset list:")
-
-print(filesHere)
-
 def add_asset_entry(stage, filename, asset_type):
     print("Adding \"" + filename + "\" to \"" + stage + "\" asset loading stage for \"" + asset_type + "\" loading method.")
-    # yikes man json_data["assets"][stage].update(filename = asset_type)
-
-def add_asset(file):
-    print("Do you want to include \"" + (file + ("" if os.path.isfile(file) == True else "/")) + "\"? (y/n)")
     while True:
-        conf = input()
-        if conf != "y" and conf != "n":
+        try:
+            json_data["assets"][stage].update({filename : asset_type})
+            break
+        except:
+            json_data["assets"][stage] = { }
+
+def add_assets_otherloader():
+    json_data["dependencies"].update({ "h3vr.otherloader.deli": "0.3.0" })
+    for file in filesHere:
+        file_split = file.split(".")
+        if len(file_split) == 1 and os.path.isfile(file):
+            add_asset_entry("runtime", file, "h3vr.otherloader.deli:item") 
+
+def add_assets_tnhtweaker():
+    json_data["dependencies"].update({ "h3vr.tnhtweaker.deli": "1.6.7" })
+    character_folder = ""
+    for file in filesHere:
+        if not os.path.isfile(file):
+            character_folder = file
+            break
+    if character_folder == "":
+        input("Could not find characer folder. Please make sure its next to me.")
+        quit()
+    else:
+        print("Adding character folder...")
+        add_asset_entry("setup", character_folder + "/", "h3vr.tnhtweaker.deli:character")
+        add_asset_entry("setup", character_folder + "/*sosig*.json", "h3vr.tnhtweaker.deli:sosig")
+        add_asset_entry("setup", character_folder + "/*vault*.json", "h3vr.tnhtweaker.deli:vault_file")
+
+def add_assets_assembly():
+    print("Adding assembly files...")
+    for file in filesHere:
+        file_split = file.split(".")
+        try:
+            if file_split[1] == "dll":
+                add_asset_entry("setup", file, "deli:assembly")
+        except:
+            continue
+    
+prompt = True
+while prompt:
+    print("What kind of files do you need to add?\n[0] - Stop adding files\n[1] - Otherloader items\n[2] - TnHTweaker Character\n[3] - Assembly")
+    while True:
+        conf = int(input())
+        if conf < 0 and conf > 3:
             print("Incorrect input, try again.")
-        elif conf == "y":
+        elif conf == 0:
+            prompt = False
             break
-        elif conf == "n":
-            return
-    filename = file
-    print("Do you need to change the name of this? (only use for globing files in custom characters) (y/n)")
-    while True:
-        conf = input()
-        if conf != "y" and conf != "n":
-            print("Incorrect input, try again.")
-        elif conf == "y":
-            filename = input("Input glob: ")
+        elif conf == 1:
+            add_assets_otherloader()
             break
-        elif conf == "n":
+        elif conf == 2:
+            add_assets_tnhtweaker()
             break
-    stage = input("Please select a loading stage. Otherloader items should be in runtime, character assets in runtime.\n[1] - \"patcher\"\n[2] - \"setup\"\n[3] - \"runtime\"\n")
-    while True:
-        if not (stage == "1" or stage == "2" or stage == "3"):
-            print("Please input a valid number.")
-        else:
-            if stage == "1":
-                stage = "patcher"
-            elif stage == "2":
-                stage = "setup"
-            else:
-                stage = "runtime"
+        elif conf == 3:
+            add_assets_assembly()
             break
-    print("What type of asset is this?\n[1] - Otherloader item\n[2] - TnHTweaker (character)\n[3] - TnHTweaker (sosig)\n[4] - TnHTweaker (vault)")
-    while True:
-        conf = input()
-        if conf <= "1" or conf > "4":
-            print("Incorrect input, try again.")
-        elif conf == "1":
-            add_asset_entry(stage, filename, "h3vr.otherloader.deli:item")
-            break
-        elif conf == "2":
-            add_asset_entry(stage, filename, "h3vr.tnhtweaker.deli:character")
-            break
-        elif conf == "3":
-            add_asset_entry(stage, filename, "h3vr.tnhtweaker.deli:sosig")
-            break
-        elif conf == "4":
-            add_asset_entry(stage, filename, "h3vr.tnhtweaker.deli:vault_file")
-            break
-        
-
-for file in filesHere:
-    add_asset(file)
-
-print(json_data["assets"])
-
-while True:
-    print("Would you like to add more assets? (y/n)")
-    conf = input()
-    if conf != "y" or conf != "n":
-        print("Incorrect input, try again.")
-    elif conf == "y":
-        add_asset(input("Please enter the path: "))
-    elif conf == "n":
-        break
-
-print("Finished adding assets. Please double check to make sure the files look correct.")
 
 print("\nFinished creating data, writing...")
 
